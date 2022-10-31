94a62c24a17626e88bec96e306d16e279ca750fb
Chris Li <chris.lty07@gmail.com>
2022-10-31 13:55:10 -0400

    commit tree dee0ec6d9894c727b43f2e2d56c5d31018227fbc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name="Arel")
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|person| badge_maker(person)}
+end 
+
+def assign_rooms(list_of_speakers)
+    list_of_speakers.map.with_index do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+    end
+end
+
+def printer(list_of_speakers)
+    batch_badge_creator(list_of_speakers).each {|badge| puts badge}
+    assign_rooms(list_of_speakers).each {|room_assignment| puts room_assignment}
+end
